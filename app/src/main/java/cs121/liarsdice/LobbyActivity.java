164623a8d913ea701bd0045618b09bcab5c8ff3d
Hardwin Bui <buihardwin@yahoo.com>
2018-06-06 22:03:39 -0700

    You can now go to the lobby screen.

--- app/src/main/java/cs121/liarsdice/LobbyActivity.java
+++ app/src/main/java/cs121/liarsdice/LobbyActivity.java
@@ -1,13 +1,32 @@
 package cs121.liarsdice;
 
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LobbyActivity extends AppCompatActivity {
+
+    WifiP2pManager myWifi;
+    boolean isHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
     }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        isHost = getIntent().getExtras().getBoolean("isHost");
+        TextView t = findViewById(R.id.textView4);
+        if(isHost)
+            t.setText("tru");
+        else
+            t.setText("trun't");
+    }
+
 }
--- app/src/main/java/cs121/liarsdice/StartActivity.java
+++ app/src/main/java/cs121/liarsdice/StartActivity.java
@@ -1,7 +1,11 @@
 package cs121.liarsdice;
 
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        // Make Lobby Button
+        Button make = (Button) findViewById(R.id.createLobbyBtn);
+        make.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(StartActivity.this, LobbyActivity.class);
+                i.putExtra("isHost", true);
+                startActivity(i);
+            }
+        });
+
+        // Join Lobby Button
+        Button join = (Button) findViewById(R.id.searchLobbyBtn);
+        join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(StartActivity.this, LobbyActivity.class);
+                i.putExtra("isHost", false);
+                startActivity(i);
+            }
+        });
     }
 }
