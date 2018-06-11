1b41f3d4f31bb2fc3234c02789493dc2bc965ba2
Justin Lau <julau@eduroam-169-233-241-147.ucsc.edu>
2018-06-11 00:56:15 -0700

    Rule pop up

--- app/src/main/java/cs121/liarsdice/RuleActivity.java
+++ app/src/main/java/cs121/liarsdice/RuleActivity.java
@@ -2,6 +2,7 @@ package cs121.liarsdice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 import java.io.Serializable;
 
@@ -11,5 +12,11 @@ public class RuleActivity extends AppCompatActivity implements Serializable {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rule);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int)(width * .8), (int) (height * .8));
     }
 }
